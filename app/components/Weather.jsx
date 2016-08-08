51da4c2bcd2e6fdd35ca8963f2fc687d5deb6937
Alvaro Muhlethaler <amuhlethaler@vairix.com>
2016-08-08 15:49:38 -0300

    Adds ErrorModal component

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var ErrorModal = require('ErrorModal');
 var openWeatherMap = require('openWeatherMap');
 
 var Weather = React.createClass({
@@ -12,7 +13,10 @@ var Weather = React.createClass({
   handleSearch: function(location){
     var that = this;
 
-    this.setState({isLoading: true});
+    this.setState({
+      isLoading: true,
+      errorMessage: undefined
+    });
 
     openWeatherMap.getTemp(location).then(function(temp){
       that.setState({
@@ -21,12 +25,14 @@ var Weather = React.createClass({
         isLoading: false
       });
     }, function(err){
-      that.setState({isLoading: false});
-      alert(err);
+      that.setState({
+        isLoading: false,
+        errorMessage: err.message
+      });
     });
   },
   render: function(){
-    var {isLoading, temp, location} = this.state;
+    var {isLoading, temp, location, errorMessage} = this.state;
 
     function renderMessage(){
       if(isLoading){
@@ -36,11 +42,20 @@ var Weather = React.createClass({
       }
     }
 
+    function renderError(){
+      if(typeof errorMessage === 'string'){
+        return (
+          <ErrorModal title="Error" message={errorMessage}/>
+        );
+      }
+    }
+
     return (
       <div>
         <h3 className="text-center">Get Weather</h3>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
+        {renderError()}
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@ module.exports = {
       Examples: 'app/components/Examples',
       WeatherForm: 'app/components/WeatherForm',
       WeatherMessage: 'app/components/WeatherMessage',
-      openWeatherMap: 'app/api/openWeatherMap'
+      openWeatherMap: 'app/api/openWeatherMap',
+      ErrorModal: 'app/components/ErrorModal'
     },
     extensions: ['','.js','.jsx']
   },
